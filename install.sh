35e3c24fa8fd978d2101850a039bc988045fd674
李梦光 <36945462+lmg231@users.noreply.github.com>
2021-09-27 14:09:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ mkdir ~/.vim/ftplugin
 echo -e "let b:ale_linters = ['flake8']\nlet b:ale_fixers = ['autopep8']" > ~/.vim/ftplugin/python.vim
 echo -e "let b:ale_linters = ['eslint']\nlet b:ale_fixers = ['eslint']" > ~/.vim/ftplugin/javascript.vim
 echo "let b:ale_linters = ['shellcheck']" > ~/.vim/ftplugin/sh.vim
-
+echo "请进入vim执行：PlugInstall后，再执行bash move.sh"
