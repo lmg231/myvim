0bcbb46e785087122a057c98b060e8908c963660
李梦光 <36945462+lmg231@users.noreply.github.com>
2021-09-27 11:51:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 move .vimrc ~
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wait
+mkdir ~/.vim/ftplugin
 echo -e "let b:ale_linters = ['flake8']\nlet b:ale_fixers = ['autopep8']" > ~/.vim/ftplugin/python.vim
 echo -e "let b:ale_linters = ['eslint']\nlet b:ale_fixers = ['eslint']" > ~/.vim/ftplugin/javascript.vim
 echo "let b:ale_linters = ['shellcheck']" > ~/.vim/ftplugin/sh.vim
+
